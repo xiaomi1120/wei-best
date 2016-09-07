d7578d190f62f83eefbcd57966652819b7ae55ab
Xuemin Han <hxm@jngongzuo.com>
2016-09-07 13:41:26 +0800

    fix css to less

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 var gulp=require('gulp');
 // 获取 uglify 模块（用于压缩 JS）
+var less=require('gulp-less');
 var uglify = require('gulp-uglify');
 // 获取 minify-css 模块（用于压缩 CSS）
-var minifyCSS = require('gulp-minify-css');
+//var minifyCSS = require('gulp-minify-css');
 
 ////用于测试gulp
 //gulp.task('default',function(){
@@ -11,18 +12,24 @@ var minifyCSS = require('gulp-minify-css');
 
 // 压缩 css 文件
 // 在命令行使用 gulp css 启动此任务
-gulp.task('css',function(){
-    //noinspection JSUnresolvedFunction
-    gulp.src('src/css/*.css')
-        .pipe(minifyCSS())
-        .pipe(gulp.dest('dist/css'))
+//gulp.task('css',function(){
+//    //noinspection JSUnresolvedFunction
+//    gulp.src('./src/assets/css/*.css')
+//        .pipe(minifyCSS())
+//        .pipe(gulp.dest('dist/css'))
+//});
+
+gulp.task('less', function() {
+    return gulp.src('./src/assets/less/index.less')
+        .pipe(less())
+        .pipe(gulp.dest('./dist/css'));
 });
 // 压缩 js 文件
 // 在命令行使用 gulp script 启动此任务
 gulp.task('script',function(){
     // 1. 找到文件
     //noinspection JSUnresolvedFunction
-    gulp.src('src/js/*.js')
+    gulp.src('./src/assets/js/*.js')
         // 2. 压缩文件
         .pipe(uglify())
         // 3. 另存压缩后的文件
@@ -32,9 +39,12 @@ gulp.task('script',function(){
 // 在命令行使用 gulp auto 启动此任务
 gulp.task('auto',function(){
     gulp.watch('js/*.js',['script']);
-    gulp.watch('css/*.css',['css']);
+    //gulp.watch('css/*.css',['css']);
+    gulp.watch('css/*.css',['less']);
 });
 
 // 使用 gulp.task('default') 定义默认任务
 // 在命令行使用 gulp 启动 script 任务和 auto 任务
-gulp.task('default', ['script','css', 'auto']);
+gulp.task('default', ['script','less', 'auto'],function (){
+    console.log('success');
+});
