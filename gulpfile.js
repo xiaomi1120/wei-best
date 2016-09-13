cec8786083c909fb786ca4ecb3d5fcd3c91ada24
Xuemin Han <hxm@jngongzuo.com>
2016-09-13 18:14:18 +0800

    add index.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,21 @@
 var gulp=require('gulp');
 // 获取 uglify 模块（用于压缩 JS）
 var less=require('gulp-less');
+var minifyCSS = require('gulp-clean-css');
 var uglify = require('gulp-uglify');
 // 获取 minify-css 模块（用于压缩 CSS）
-//var minifyCSS = require('gulp-minify-css');
-
+var rename = require('gulp-rename');
+var connect = require('gulp-connect');
 ////用于测试gulp
 //gulp.task('default',function(){
 //console.log("你好")
 //});
-
+gulp.task('webserver', function() {
+    connect.server({
+        livereload: true,
+        port: 2333
+    });
+});
 // 压缩 css 文件
 // 在命令行使用 gulp css 启动此任务
 //gulp.task('css',function(){
@@ -19,10 +25,18 @@ var uglify = require('gulp-uglify');
 //        .pipe(gulp.dest('dist/css'))
 //});
 
-gulp.task('less', function() {
-    return gulp.src('./src/assets/less/index.less')
+gulp.task('lessWei', function() {
+     //noinspection JSUnresolvedFunction
+    gulp.src('./src/assets/less/index.less')
         .pipe(less())
-        .pipe(gulp.dest('./dist/css'));
+        .pipe(minifyCSS())
+        .pipe(rename({
+            dirname:'/',
+            extname:'.min.css'
+        }))
+        .pipe(gulp.dest('./dist/css'))
+        .pipe(connect.reload());
+
 });
 // 压缩 js 文件
 // 在命令行使用 gulp script 启动此任务
@@ -40,11 +54,11 @@ gulp.task('script',function(){
 gulp.task('auto',function(){
     gulp.watch('js/*.js',['script']);
     //gulp.watch('css/*.css',['css']);
-    gulp.watch('css/*.css',['less']);
+    gulp.watch('./src/assets/less/*.less',['lessWei']);
 });
 
 // 使用 gulp.task('default') 定义默认任务
 // 在命令行使用 gulp 启动 script 任务和 auto 任务
-gulp.task('default', ['script','less', 'auto'],function (){
+gulp.task('default', ['script','lessWei', 'auto','webserver'],function (){
     console.log('success');
 });
